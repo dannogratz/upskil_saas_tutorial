77e6cb5f8d4c2a15d4c9af6bdaff06f0d30cea88
Grodis Kostas <grodiscostas@hotmail.com>
2017-02-27 17:14:51 +0000

    Changed email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'grodiscostas@hotmail.com'
+  default to: 'grodiscostas@gmail.com'
   
   def contact_email(name, email, body)
     @name = name
